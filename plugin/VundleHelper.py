8a1e53563faa6834a872ab418e92bbafdfeaed55
Jonathan <jonathandavis@gilsons.org>
2014-12-17 00:52:37 -0500

    Use last update as check for vundlehelper update

--- plugin/VundleHelper.py
+++ plugin/VundleHelper.py
@@ -136,7 +136,7 @@ def VundleHelper_run_updates():
 def VundleHelper_self_update():
     one_day = 1 * 24 * 60 * 60
     dates = VundleHelper_read_update_cache()
-    next = VundleHelper_get_next_update(dates)
+    next = VundleHelper_get_last_update(dates)
     freq = VundleHelper_update_how_often()
     if next < time.time() + one_day:
         s = Self_update.Self_update()
