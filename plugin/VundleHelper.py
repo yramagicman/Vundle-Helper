8594381d060dbc2681f4a4fe3427af4b11c5998f
Jonathan <jonathandavis@gilsons.org>
2015-01-22 16:25:53 -0500

    maybe this will work now

--- plugin/VundleHelper.py
+++ plugin/VundleHelper.py
@@ -139,7 +139,7 @@ def VundleHelper_run_updates():
     dates = VundleHelper_read_update_cache()
     next = VundleHelper_get_next_update(dates)
     freq = VundleHelper_update_how_often()
-    if next > time.time():
+    if float(time.time()) > float(next):
         vim.command('PluginUpdate')
         VundleHelper_write_last_update(freq)
 
